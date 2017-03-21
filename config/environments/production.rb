5f6060b23645b21d726d25971f9d72923de3aad3
lgor89 <igor.penkovsky@gmail.com>
2017-03-21 20:14:48 +0300

    check mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
-      domain: 'sellasaur.com',
+      domain: 'simple-blog-igor-24.herokuapp.com',
       authentication: :plain,
       enable_starttls_auto: true,
       user_name: ENV['GMAIL_USERNAME'],
